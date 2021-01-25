690a53d6a593a8aeda5023941a0f5a3bead8271c
Daniel Soares de Albuquerque <64704136+App-Almoxarifado@users.noreply.github.com>
2021-01-25 14:57:22 -0300

    clone

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default function Home() {
-  return <div>Clone youtube com Next.js + Material-UI</div>;
+  return (
+    <div>
+      Clone youtube com Next.js + Material-UI
+      <Button variant="contained" size="small" color="primary">
+        Small
+      </Button>
+      <Button variant="contained" size="medium" color="primary">
+        Medium
+      </Button>
+      <Button variant="contained" size="large" color="primary">
+        Large
+      </Button>
+    </div>
+  );
 }
