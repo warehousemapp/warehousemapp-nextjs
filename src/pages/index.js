12487cc0a339a2b1707d79355056a66302b23b4d
Daniel Soares de Albuquerque <64704136+App-Almoxarifado@users.noreply.github.com>
2021-01-25 15:55:38 -0300

    color primary

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 export default function Home() {
   return (
     <div>
-      <h1>Clone youtube com Next.js + Material-UI</h1>
+      <h1>Clone youtube com Next.js</h1>
       <Button variant="contained" size="small" color="primary">
         Small
       </Button>
